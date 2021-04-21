9a04b34d28487876d194b34f3afe0dca3c01ad9f
ViniciusFloriano <viniciusfloriano05@gmail.com>
2021-04-20 23:37:50 -0300

    Home Page

---
+++ app/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function Footer() {
+  return (
+      <div className="footer">
+          <span>Project made by Vinicius Floriano</span>
+      </div>
+  );
+}
+
+export default Footer;
--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Footer from '../../components/Footer';
+import Search from '../../assets/search.png';
+
+
+import './styles.css';
 
 function Home() {
-  return <div />;
+  return (
+    <div className="content">
+      <div className="home">
+        <h1>Search Devs</h1>
+        <form>
+          <input placeholder="Type the username here..." />
+          <button>
+            <img alt="Search" src={Search}></img>
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
