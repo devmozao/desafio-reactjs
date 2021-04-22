7afae90adf9f3376faa0f40b23d056a4eb897487
ViniciusFloriano <viniciusfloriano05@gmail.com>
2021-04-22 18:50:36 -0300

    Error message and css

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -4,13 +4,21 @@ import Footer from '../../components/Footer';
 import Search from '../../assets/search.png';
 
 import './styles.css';
+import api from '../../services/api';
 
 function Home() {
   const history = useHistory();
   const [ username, setUsername ] = useState('');
+  const [ errorMessage, setErrorMessage ] = useState('');
 
-  function handleSearch() {
-    history.push(`/profile/${username}`)
+  function handleSearch(e) {
+    e.preventDefault();
+    api.get(`/users/${username}`).then(({data}) => {
+      setErrorMessage('');
+      history.push(`/profile/${username}`);
+    }).catch(() => {
+      setErrorMessage('Usuário não encontrado.');
+    });
   }
 
   return (
@@ -23,6 +31,7 @@ function Home() {
             <img alt="Search" src={Search}></img>
           </button>
         </form>
+        {errorMessage && <span className="error-message">{errorMessage}</span>}
       </div>
       <Footer />
     </div>
