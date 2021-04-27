8b6a041dd63162929f56511cd0831efac8ef40ff
Guilherme Pupo <guilh.pupo@gmail.com>
2021-04-26 23:36:29 -0300

    eslintrc adjustments

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   border: {
     radius: '0.5rem'
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,9 +4,9 @@ export function formatUpdatedAt(value: string) {
   const lastUpdateDate = new Date(value);
   const todayDate = new Date();
 
-  let timeDifference = todayDate.getTime() - lastUpdateDate.getTime();
+  const timeDifference = todayDate.getTime() - lastUpdateDate.getTime();
 
-  let dayDifference = timeDifference / (1000 * 3600 * 24);
+  const dayDifference = timeDifference / (1000 * 3600 * 24);
 
   return `${dayDifference.toFixed(0.0)} days ago`;
 }
